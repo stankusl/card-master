d335f550395669f9123733ef980b9238c6816198
Exe1 <stankusl@gmail.com>
2016-10-06 08:30:32 +0100

    latest

--- src/app/components/game/game-controller.js
+++ src/app/components/game/game-controller.js
@@ -1,7 +1,14 @@
 (function() {
     'use strict';
     angular.module('application').controller('GameController', function($rootScope, $scope, $log, $q, GameServices) {
+        self = this;
         $rootScope.pageTitle = 'Home';
+
+        self.cards = {};
+
+        GameServices.getCards().then(function(data) {
+          self.cards = data;
+        });
 
 
 
